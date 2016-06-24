e4ddfe413c9a7efceb463181ebb64a37b9f125fd
rodrigo valdez <rodri.valdez@gmail.com>
2016-06-23 21:32:36 -0400

    first commit

---
+++ presupuesto_ciudadano/main/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url,include
+
+from . import views
+
+
+from django.conf.urls import url, include
+
+from main import views
+
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^financiacion/$', views.financiacion, name='financiacion'),
+    url(r'^ciclo/$', views.ciclo, name='ciclo'),
+	url(r'^gastos/$', views.gastos, name='gastos'),
+	url(r'^ejecucion/$', views.ejecucion, name='ejecucion'),
+   
+
+]
---
+++ presupuesto_ciudadano/main/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, render_to_response
+
+from django.http import HttpResponse
+
+from django.contrib.auth.models import User, Group
+
+
+
+import json
+from django.core import serializers
+
+
+
+def index(request): 
+    return render_to_response('index.html')
+
+
+def financiacion(request):      
+    return render_to_response('financiacion.html')
+
+
+def ciclo(request):      
+    return render_to_response('ciclo.html')
+
+
+def gastos(request):      
+    return render_to_response('gastos.html')
+
+
+def ejecucion(request):      
+    return render_to_response('ejecucion.html')
